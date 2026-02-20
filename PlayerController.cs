a79f9f9016c7b0b7013317988d4df057bcc119ac
Sa <salvarezmonzon@danielcastelao.org>
2026-02-20 10:41:56 +0100

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forceAmount = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * forceAmount);
+    }
+}
